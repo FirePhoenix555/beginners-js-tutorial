973606c41d9802f97560b571129705e281eb869d
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2024-08-07 10:10:51 -0500

    added basic js and html

---
+++ base.js
@@ -0,0 +1,12 @@
+// The function setup() is run at program initiation
+function setup() {
+    // Create the canvas with specified width and height
+    createCanvas(400, 400);
+}
+
+// The draw() function you're used to
+function draw() {
+    background(0);
+}
+
+// YOUR CODE HERE
